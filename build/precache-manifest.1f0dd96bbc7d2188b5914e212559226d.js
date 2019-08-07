b908305083d4570740560d721de3bc7b7e4d4c3b
lybomyr7 <zdrilyiliubomyr@gmail.com>
2019-08-07 23:07:33 +0300

    last

---
+++ build/precache-manifest.1f0dd96bbc7d2188b5914e212559226d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761bc9df1de54be7292a8dfbfdeb9520",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4262cd2bd321ff2cb6d6",
+    "url": "/static/css/main.3615b207.chunk.css"
+  },
+  {
+    "revision": "7d6b5e66cd522e3efd39",
+    "url": "/static/js/2.4f1e0f65.chunk.js"
+  },
+  {
+    "revision": "4262cd2bd321ff2cb6d6",
+    "url": "/static/js/main.c7a5c397.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9277f7c9afcefc754228dfbc195385a2",
+    "url": "/static/media/market-share-468x258.9277f7c9.jpg"
+  }
+]);
--- server/main.js
+++ server/main.js
@@ -31,7 +31,7 @@ app.use('/',userRouter);
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
